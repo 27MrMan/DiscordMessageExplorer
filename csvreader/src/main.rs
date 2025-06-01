e6e0f9f75d8af7ae103a757ca6d98d36ba803059
27MrMan <gautamkrishus1@gmail.com>
2025-06-01 18:32:53 +0530

    Added some basic rust code right now it just to test whether the rust stuff works on my PC, eventually ill add actual functional code.

--- csvreader/src/main.rs
+++ csvreader/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut rdr = csv::Reader::from_path("data_copy.csv").expect("wher file");
+    let mut limit = 150;
+
+    for i in rdr.records() {
+        let line = i.expect("wher record");
+        println!("{:?}", line);
+        println!(" ");
+        limit -=1;
+        if limit == 0 {
+            break;
+        }
+    }
 }
