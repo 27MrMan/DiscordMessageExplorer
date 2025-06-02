739388dc34c24bdbb852c370d793379d5c765556
27MrMan <gautamkrishus1@gmail.com>
2025-06-02 21:12:01 +0530

    Checked some more rust stuff turns out if i build with --release, it only takes 40 ms to go through the entire 159000 messages. There is no processing yet though.

--- csvreader/src/main.rs
+++ csvreader/src/main.rs
@@ -1,14 +1,27 @@
+use std::time::{SystemTime};
+
 fn main() {
     let mut rdr = csv::Reader::from_path("data_copy.csv").expect("wher file");
     let mut limit = 150;
+    let mut print:bool = true;
+    let mut lentest = 0;
 
+    let starttime = SystemTime::now();
     for i in rdr.records() {
         let line = i.expect("wher record");
+        if print{
         println!("{:?}", line);
-        println!(" ");
+        println!(" ");}
+
         limit -=1;
+        lentest += 1;
+
         if limit == 0 {
-            break;
+            print = false;
         }
+
+
     }
+    println!("Total records processed: {}", lentest);
+    println!("Time Elapsed: {:?}", starttime.elapsed());
 }
